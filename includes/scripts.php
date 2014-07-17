db4d918489e9a65c82fe1bd2fdfcd258429044ab
Travis Pflanz <tpflanz@webworksofkc.com>
2014-07-17 11:42:01 -0500

    Recent Popular Tags
    added plugin files

---
+++ includes/scripts.php
@@ -0,0 +1,10 @@
+<?php
+
+/***** Script Control *******/
+
+function rpt_load_scripts() {
+	if(is_single()){
+		wp_enqueue_style('rpt-styles', plugin_dir_url(__FILE__) . 'css/plugin_styles.css');
+	}
+}
+add_action('wp_enqueue_scripts', 'rpt_load_scripts');
---
+++ recent-popular-tags.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Recent Popular Tags
+ * Plugin URI: http://webworksofkc.com
+ * Description: Display a list of the 28 most popular post tags over the last 30 days
+ * Version: 0.1
+ * Author: Travis Pflanz
+ * Author URI: http://Travis.Pflanz.ME
+ * License: GPL2
+ */
+
+/******* Global Variables **********/ 
+$rpt_options = get_option('rpt_settings');
+/******* Includes **********/ 
+
+include('includes/scripts.php');
+include('includes/display-functions.php');
+include('includes/admin-page.php');
