66c64ae991788bc00c41f7474b4125a015a0a1b9
Travis Pflanz <tpflanz@webworksofkc.com>
2014-07-17 12:00:59 -0500

    Update scripts.php

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,29 +1,12 @@
 <?php
 
-
-
-
-
 /***** Script Control *******/
 
-
-
-
-
 function rpt_load_scripts() {
-
-
 	if(is_single()){
-
-
 		wp_enqueue_style('rpt-styles', plugin_dir_url(__FILE__) . 'css/rpt_styles.css');
-
-
 	}
-
-
 }
-
 
 add_action('wp_enqueue_scripts', 'rpt_load_scripts');
 ?>
